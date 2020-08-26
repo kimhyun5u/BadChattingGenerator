0e1dbdffa2c67e89c2062084fb0625ecb4e08980
sasiman02 <gustndlfrl@gmail.com>
2020-08-27 03:49:06 +0900

    changing to auto-complete chat

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
+let result;
 function generator() {
-  var chat = document.querySelector('#chat').value;
-  var n = document.querySelector('#n').value;
-  var result = "";
-  for(var i = 0; i < n; i++) {
+  let chat = document.querySelector('#chat').value;
+  let n = document.querySelector('#n').value;
+  result = "";
+  for(let i = 0; i < n; i++) {
     result += chat;
   }
   document.querySelector('#output').innerText = result;
   navigator.clipboard.writeText(result);
+  chrome.tabs.executeScript(
+    {file: "chat.js"  
+  });
 }
+
 
 document.addEventListener('DOMContentLoaded', function() {
   var convert = document.querySelector('#convert');
