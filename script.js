68711e13137fac27d428584b7da796a9c55aad39
sasiman02 <gustndlfrl@gmail.com>
2021-06-09 16:05:20 +0900

    migrate to M3
    	새 파일:       background.js
    	새 파일:       image/128.png
    	수정함:        manifest.json
    	수정함:        popup.html
    	수정함:        script.js
    	삭제함:        128.png
    	삭제함:        chat.js

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
 let result;
 function generator() {
-  let chat = document.querySelector('#chat').value;
-  let n = document.querySelector('#n').value;
+  let chat = document.getElementById('chat').value;
+  let n = document.getElementById('n').value;
   result = "";
-  for(let i = 0; i < n; i++) {
+  for (let i = 0; i < n; i++) {
     result += chat;
   }
-  document.querySelector('#output').innerText = result;
+
+  document.getElementById('output').innerText = result;
   navigator.clipboard.writeText(result);
   chrome.tabs.executeScript(
-    {file: "chat.js"  
-  });
+    {
+      file: "chat.js"
+    });
 }
 
 
-document.addEventListener('DOMContentLoaded', function() {
-  var convert = document.querySelector('#convert');
+document.addEventListener('DOMContentLoaded', function () {
+  var convert = document.getElementById('convert');
   convert.addEventListener("click", generator);
 });
+
+function setAnalytics() {
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-XXXXXXXX-X']);
+  _gaq.push(['_trackPageview']);
+
+  (function () {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+};
+
+setAnalytics();
+
+function trackButton(e) {
+  _gaq.push(['_trackEvent', e.target.id, 'clicked']);
+};
+
+var buttons = document.querySelectorAll('button');
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', trackButtonClick);
+} 
