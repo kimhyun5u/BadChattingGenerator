b70ede53c9652f637ddb8a5152c984f29f438a96
sasiman92 <gustndlfrl@gmail.com>
2021-06-11 20:08:35 +0900

    악질채팅생성기 v1.1.0

--- script.js
+++ script.js
@@ -20,27 +20,3 @@ document.addEventListener('DOMContentLoaded', function () {
   var convert = document.getElementById('convert');
   convert.addEventListener("click", generator);
 });
-
-function setAnalytics() {
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-XXXXXXXX-X']);
-  _gaq.push(['_trackPageview']);
-
-  (function () {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = 'https://ssl.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-};
-
-setAnalytics();
-
-function trackButton(e) {
-  _gaq.push(['_trackEvent', e.target.id, 'clicked']);
-};
-
-var buttons = document.querySelectorAll('button');
-for (var i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', trackButtonClick);
-} 
