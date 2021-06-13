66da3f0e46ddfefe524e9cb478db784da99c6073
sasiman02 <gustndlfrl@gmail.com>
2021-06-13 12:57:24 +0900

    악질채팅생성기 v1.1.0 (재업)

--- chat.js
+++
@@ -1,3 +0,0 @@
-var chatInput = document.querySelector('.chat-input textarea');
-chatInput.value = "testing...";
-chatInput.dispatchEvent(new Event('input', { bubbles: true }));  
--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 let result;
-function generator() {
-  let chat = document.getElementById('chat').value;
-  let n = document.getElementById('n').value;
-  result = "";
-  for (let i = 0; i < n; i++) {
-    result += chat;
-  }
 
-  document.getElementById('output').innerText = result;
-  navigator.clipboard.writeText(result);
-  chrome.tabs.executeScript(
-    {
-      file: "chat.js"
-    });
+function generator() {
+    let chat = document.getElementById('chat').value;
+    let n = document.getElementById('n').value;
+    result = "";
+    for (let i = 0; i < n; i++) {
+        result += chat;
+    }
+
+    document.getElementById('output').innerText = result;
+    navigator.clipboard.writeText(result);
 }
 
 
-document.addEventListener('DOMContentLoaded', function () {
-  var convert = document.getElementById('convert');
-  convert.addEventListener("click", generator);
+document.addEventListener('DOMContentLoaded', function() {
+    var convert = document.getElementById('convert');
+    convert.addEventListener("click", generator);
 });
